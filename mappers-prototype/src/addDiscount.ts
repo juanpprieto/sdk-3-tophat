226d37dec440f03043534d6608f6f0a9c36446a5
Juan P. Prieto <jp@calltheguys.co>
2024-11-04 09:44:27 -0800

    Add addDiscount input mapper

---
+++ mappers-prototype/src/addDiscount.ts
@@ -0,0 +1,18 @@
+import { MutationcheckoutDiscountCodeApplyV2Args } from './types/2024-04'
+import { MutationcartDiscountCodesUpdateArgs  } from './types/2024-10'
+
+type AddDiscountInputMapper = {
+  cartId: MutationcartDiscountCodesUpdateArgs['cartId'],
+  discountCodes: MutationcartDiscountCodesUpdateArgs['discountCodes']
+}
+
+export const addDiscountInputMapper = (
+  checkoutId: MutationcheckoutDiscountCodeApplyV2Args['checkoutId'],
+  discountCode: MutationcheckoutDiscountCodeApplyV2Args['discountCode']
+): AddDiscountInputMapper => {
+   return { 
+    cartId: checkoutId,
+    // NOTE: checkout support only one discount code
+    discountCodes: discountCode ? [discountCode] : []
+  }
+}
